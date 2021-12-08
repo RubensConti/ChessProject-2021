bb5de9eb420a4fdff7ac635769fc11a5c0ba7d8c
RubensConti <85520679+RubensConti@users.noreply.github.com>
2021-12-07 22:08:41 -0300

    Add files via upload

--- Projeto2/src/com/chess/JavaChess.java
+++ Projeto2/src/com/chess/JavaChess.java
@@ -1,16 +1,10 @@
 package com.chess;
 
-import com.chess.engine.board.Board;
-import gui.StartScreen;
+import com.chess.board.Board;
 
 public class JavaChess {
-
-
     public static void main(String[] args){
         Board board = Board.createStandardBoard();
         System.out.println(board);
-
-
-        StartScreen startScreen = new StartScreen();
     }
 }
